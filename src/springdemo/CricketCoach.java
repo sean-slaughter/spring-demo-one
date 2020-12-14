eb18131846c942cc719c258db97d7c9d4be6b9bf
sean-slaughter <sthompson2822@gmail.com>
2020-12-14 13:45:05 -0600

    Use setter injection to inject literal values

--- src/springdemo/CricketCoach.java
+++ src/springdemo/CricketCoach.java
@@ -3,6 +3,8 @@ package springdemo;
 public class CricketCoach implements Coach {
 	
 	private FortuneService fortuneService;
+	private String emailAddress;
+	private String team;
 
 	public CricketCoach() {
 		System.out.println("Cricket coach constructor");
@@ -19,6 +21,20 @@ public class CricketCoach implements Coach {
 	public void setFortuneService(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
 	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	public String getTeam() {
+		return team;
+	}
+	public void setTeam(String team) {
+		this.team = team;
+	}
+	
+	
 	
 	
 
--- src/springdemo/SetterDemoApp.java
+++ src/springdemo/SetterDemoApp.java
@@ -12,6 +12,8 @@ public class SetterDemoApp {
 		//call methods on the bean
 		System.out.println(theCoach.getDailyFortune());
 		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getEmailAddress());
+		System.out.println(theCoach.getTeam());
 		//close the context
 		context.close();
 	}
