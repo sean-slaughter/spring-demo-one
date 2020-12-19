f623c0f9cd0bd4406d0a017ea65dcb9a0a54e88c
sean-slaughter <sthompson2822@gmail.com>
2020-12-19 11:00:47 -0600

    Create new xml config file for learning scope

---
+++ src/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,17 @@
+package springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		//load the spring config file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+		//retrieve beans from the spring container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		Coach alphaCoach = context.getBean("myCoach", Coach.class);
+
+		
+	}
+
+}
